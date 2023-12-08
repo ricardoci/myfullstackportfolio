79596a8ebfbf44bb8ffe15d033939f6ea0d8b166
unknown <ricardocisneros197@gmail.com>
2023-12-07 17:47:58 -0800

    a

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve the PDF file
+app.get('/FullStackResume.pdf', (req, res) => {
+  const filePath = path.join(__dirname, 'FullStackResume.pdf');
+  res.setHeader('Content-Disposition', 'attachment; filename="FullStackResume.pdf"');
+  res.setHeader('Content-Type', 'application/pdf');
+  res.sendFile(filePath);
+});
+
+// Serve the index.html file for all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+// Start the server
+const port = process.env.port || 3001;
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
